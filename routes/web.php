16adc029750010b8c859586f8e8770d486ef01f2
MD Enamul Haque <eng.enamulcse@gmail.com>
2025-01-19 12:56:19 +0600

    check message bug

--- routes/web.php
+++ routes/web.php
@@ -19,21 +19,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {     
-    $student_register = StudentRegister::where('user_id', 215)->first();
-        if ($student_register) {
-            $phone_number = $student_register->user->phone;
-            $message = "সম্মানিত অভিভাবক! আলহামদুলিল্লাহ, আপনার দেয়া তথ্য অনুযায়ী আপনার সন্তান: {$student_register->name} নিবন্ধন নাম্বার: {$student_register->reg_id} পরবর্তী ধাপের জন্য নির্বাচিত হয়েছে।
-                        আপনার সন্তানের দ্বীনী ইলম হাছিলের এ মহান সাধনায় আপনার এবং আপনার ঘরের মন মানসিকতা ও পরিবেশের রয়েছে অপরিসীম ভূমিকা। তাই এ বিষয়েও আমরা আপনার কাছে কিছু জানতে চাই। নিচের ঠিকানায় প্রবেশ করে সুচিন্তিতভাবে উত্তরগুলো লিখে পাঠান। আল্লাহ তাওফিক দান করুন https://admission.mimalmadinah.com/admission-form/step-3?user_id={$student_register->user_id}";
-
-            try {
-                $message_service = new PhoneMessageService;
-                return $message_service->sendMessage($phone_number, $message); 
-            } catch (Exception $e) {
-                return error_response( $e->getMessage(), 500); 
-            }
-        } else {
-            return error_response('Invalid Registration Number', 404);
-        }
+     $messageService = new PhoneMessageService;
+     $message = "সম্মানিত অভিভাবক! আলহামদুলিল্লাহ, আপনার দেয়া তথ্য অনুযায়ী আপনার সন্তান: Enamul নিবন্ধন নাম্বার: 3654 পরবর্তী ধাপের জন্য নির্বাচিত হয়েছে।
+                        আপনার সন্তানের দ্বীনী ইলম হাছিলের এ মহান সাধনায় আপনার এবং আপনার ঘরের মন মানসিকতা ও পরিবেশের রয়েছে অপরিসীম ভূমিকা। তাই এ বিষয়েও আমরা আপনার কাছে কিছু জানতে চাই। নিচের ঠিকানায় প্রবেশ করে সুচিন্তিতভাবে উত্তরগুলো লিখে পাঠান। আল্লাহ তাওফিক দান করুন https://admission.mimalmadinah.com/admission-form/step-3?user_id=1";
+     return $messageService->sendMessage(+8801796351081, $message);
 });
 
 Route::get("sendMessage",[InterviewController::class,'sendSms']);
