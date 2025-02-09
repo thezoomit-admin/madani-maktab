60ab7c8b9082f654b293ee5321160fecf59943de
Md Enamul Haque <enamul807849@gmail.com>
2025-02-09 09:01:12 +0600

    solving dashboard proble

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -25,46 +25,20 @@ class DashboardController extends Controller
             ->get();  
     }
  
-    /**
- * Get the counts for various student statuses.
- *
- * @param \Illuminate\Support\Collection $students
- * @return array
- */
     private function getStudentCounts($students)
     {
         return [
             'total_application' => $students->count(),
-            'general_fail' => $students->whereHas('admissionProgress', function($q) {
+            'general_fail' => $students->whereHas(function($q){
                 $q->where('is_passed_age', 0);
             })->count(), 
 
-            'general_pass' => $students->whereHas('admissionProgress', function($q) {
-                $q->where('is_passed_age', 1);
-            })->count(), 
-
-            'interview_fail' => $students->whereHas('admissionProgress', function($q) {
-                $q->where('is_passed_age', 1)->where('is_passed_interview', 0);
-            })->count(), 
-
-            'interview_pass' => $students->whereHas('admissionProgress', function($q) {
-                $q->where('is_passed_age', 1)->where('is_passed_interview', 1);
-            })->count(), 
-
-            'final_fail' => $students->whereHas('admissionProgress', function($q) {
-                $q->where('is_passed_age', 1)
-                ->where('is_passed_interview', 1)
-                ->where('is_passed_trial', 0);
-            })->count(), 
-
-            'final_pass' => $students->whereHas('admissionProgress', function($q) {
-                $q->where('is_passed_age', 1)
-                ->where('is_passed_interview', 1)
-                ->where('is_passed_trial', 1);
-            })->count(), 
-            
+            'general_pass' => $students->where('is_passed_age', 1)->count(),
+            'interview_fail' => $students->where('is_passed_age', 1)->where('is_passed_interview', 0)->count(),
+            'interview_pass' => $students->where('is_passed_age', 1)->where('is_passed_interview', 1)->count(),
+            'final_fail' => $students->where('is_passed_age', 1)->where('is_passed_interview', 1)->where('is_passed_trial', 0)->count(),
+            'final_pass' => $students->where('is_passed_age', 1)->where('is_passed_interview', 1)->where('is_passed_trial', 1)->count(),
         ];
     }
-
 
 }
