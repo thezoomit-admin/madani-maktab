24e2e223709f0f9fe0a304be3cdbc24ed943e9f1
Md Enamul Haque <enamul807849@gmail.com>
2025-07-15 18:25:29 +0600

    update expense report

--- app/Http/Controllers/Report/DueReportController.php
+++ app/Http/Controllers/Report/DueReportController.php
@@ -36,24 +36,24 @@ class DueReportController extends Controller
         $endDate = $dates->end_date;
 
         $vendors = Vendor::select('name','id')
-            ->withCount([
-                'expenses as total_expense' => function ($query) use ($startDate, $endDate) {
-                    $query->whereBetween('created_at', [$startDate, $endDate])
-                        ->select(DB::raw("COALESCE(SUM(total_amount), 0)"));
-                },
-                'payments as total_payment' => function ($query) use ($startDate, $endDate) {
-                    $query->whereBetween('created_at', [$startDate, $endDate])
-                        ->select(DB::raw("COALESCE(SUM(amount), 0)"));
-                }
-            ])
+            // ->withCount([
+            //     'expenses as total_expense' => function ($query) use ($startDate, $endDate) {
+            //         $query->whereBetween('created_at', [$startDate, $endDate])
+            //             ->select(DB::raw("COALESCE(SUM(total_amount), 0)"));
+            //     },
+            //     'payments as total_payment' => function ($query) use ($startDate, $endDate) {
+            //         $query->whereBetween('created_at', [$startDate, $endDate])
+            //             ->select(DB::raw("COALESCE(SUM(amount), 0)"));
+            //     }
+            // ])
             ->get()
             ->map(function ($vendor) {
                 return [
                     'id' => $vendor->id,
                     'name' => $vendor->name,
-                    'total_expense' => $vendor->total_expense,
-                    'total_payment' => $vendor->total_payment,
-                    'total_due' => $vendor->total_expense - $vendor->total_payment,
+                    // 'total_expense' => $vendor->total_expense,
+                    // 'total_payment' => $vendor->total_payment,
+                    'total_due' => $vendor->due,
                 ];
             });
 
