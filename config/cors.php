967e25568c12c998e4fb6cb0d2e40673bbae6cab
MD Enamul Haque <enamul807849@gmail.com>
2026-01-22 11:27:50 +0600

    update cors

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [ 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    'allowed_origins' => ['http://localhost:3003', 'http://localhost:3000', 'http://erp.mimalmadinah.com', 'https://erp.mimalmadinah.com'],
+    'allowed_origins' => ['http://localhost:3003', 'http://localhost:3000', 'http://erp.mimalmadinah.com', 'https://erp.mimalmadinah.com','https://www.mimalmadinah.com','https://mimalmadinah.com','http://localhost:5173'],
 
     'allowed_origins_patterns' => [],
 
