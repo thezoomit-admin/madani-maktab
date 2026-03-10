afb0372dff5360dbc317e48b7d13099755f60056
MD Enamul Haque <enamul807849@gmail.com>
2026-03-10 10:41:08 +0600

    update

--- app/Http/Controllers/Admin/Admission/DecisionChangeController.php
+++ app/Http/Controllers/Admin/Admission/DecisionChangeController.php
@@ -12,27 +12,33 @@ class DecisionChangeController extends Controller
     public function notInterested(Request $request){
         $candidate_id = $request->candidate_id;
         $progress = AdmissionProgressStatus::where('user_id', $candidate_id)->first();
-        if($progress){
-            $progress->is_passed_age = false;
-            $progress->save();
+        if(!$progress){
+            return error_response(null, 404, 'ডেটা পাওয়া যায়নি।');
         }
+        $progress->is_interested = false;
+        $progress->save();
+
         $student = StudentRegister::where('user_id', $candidate_id)->first();
         if($student){
             $student->note = $student->note." আগ্রহ প্রকাশ করেনি: ".$request->note;
             $student->save();
         }
+        return success_response(null, 'আপডেট সফল হয়েছে।');
     }
     public function interested(Request $request){
         $candidate_id = $request->candidate_id;
         $progress = AdmissionProgressStatus::where('user_id', $candidate_id)->first();
-        if($progress){
-            $progress->is_passed_age = true;
-            $progress->save();
+        if(!$progress){
+            return error_response(null, 404, 'ডেটা পাওয়া যায়নি।');
         }
+        $progress->is_interested = true;
+        $progress->save();
+
         $student = StudentRegister::where('user_id', $candidate_id)->first();
         if($student){
             $student->note = $student->note." আগ্রহ প্রকাশ করেছে: ".$request->note;
             $student->save();
         }
+        return success_response(null, 'আপডেট সফল হয়েছে।');
     }
 }
